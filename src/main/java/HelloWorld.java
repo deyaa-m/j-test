f56b256f853928c491e375e53f16b951288a412f
Deya' Abo El-ata <50682267+deyaa-m@users.noreply.github.com>
2019-09-24 18:37:57 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/", (req, res) -> "  trial 2  !!  ");
+		get("/", (req, res) -> "  trial 3  !!  ");
 	}
 }
