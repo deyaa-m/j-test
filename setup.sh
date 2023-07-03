b8d43ba9fbd1b51f7c2323c335b8df04e33eea89
Deyaa M <deyaaabolata@gmail.com>
2023-07-03 16:00:51 +0300

    testing packer pub repo

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+location=~/.ssh/git-key
+
+sudo apt update
+sudo apt install git -y
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+echo "==================================== ssh key"
+echo $location
+
+sleep 5
+
+echo "==================================== clone pub"
+git clone git@github.com:deyaa-m/j-test.git ~/app
+
+ls -al ~/app
+
+
